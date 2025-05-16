c6b218b1b5151aa39072a18c2cb3521ba8a9036c
pedroivo1 <pedroivoal1@gmail.com>
2025-05-15 21:40:59 -0300

    P-SET9

--- Problem List/9. Palindrome Number.c
+++ Problem List/9. Palindrome Number.c
@@ -1,32 +1,50 @@
-#include <stdio.h>
 #include <stdbool.h>
-#include <stddef.h>
-
-size_t strLen(const char *str)
-{
-    size_t length = 0;
-    while (str[length] != '\0')
-    {
-        length++;
-    }
-    return length;
-}
 
 bool isPalindrome(int x)
 {
     if (x < 0 || x%10 == 0 && x != 0) return false;
-
-    char str[11];
-    snprintf(str, sizeof(str), "%d", x);
-    int length = strLen(str);
-    int i = 0;
-    while (i < length / 2)
+    int i, d;
+    d = x;
+    unsigned int j = 0;
+    while (d > 0 && d != 0)
     {
-        if (str[length - 1 - i] != str[i])
-        {
-            return false;
-        }
-        i++;
+        i = d % 10;
+        j = j*10 + i;
+        d = d/10;
     }
-    return true;
+
+    if (j == x) return true;
+    return false;
 }
+
+
+
+// USING STRING
+// size_t strLen(const char *str)
+// {
+//     size_t length = 0;
+//     while (str[length] != '\0')
+//     {
+//         length++;
+//     }
+//     return length;
+// }
+
+// bool isPalindrome(int x)
+// {
+//     if (x < 0 || x%10 == 0 && x != 0) return false;
+
+//     char str[11];
+//     snprintf(str, sizeof(str), "%d", x);
+//     int length = strLen(str);
+//     int i = 0;
+//     while (i < length / 2)
+//     {
+//         if (str[length - 1 - i] != str[i])
+//         {
+//             return false;
+//         }
+//         i++;
+//     }
+//     return true;
+// }
